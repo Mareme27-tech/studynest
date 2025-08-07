a82c817d7c8bd8a34ac17dcf6c2ec643c59625b1
Doyen Simango <1602758@students.wits.ac.za>
2025-08-07 23:29:53 +0200

    To allow frontend to call api

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+   const app = await NestFactory.create(AppModule);
+   
+app.enableCors({
+  origin: 'http://localhost:4200',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
+
+
   await app.listen(process.env.PORT ?? 3000);
+
+
 }
 bootstrap();
